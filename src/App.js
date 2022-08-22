a2319ecdde272c4087627c1632714d4ad6318f56
Alex <herbalpie@gmail.com>
2022-08-22 18:58:53 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import products from './components/Products';
+import {Card, Row, Col} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='text-center'>
+      <h3>Sneakers</h3>
     </div>
+    <div className='container bg-light mx-auto m-3 p-3 rounded'>
+        <Row>
+            {products.map(product =>
+            <Col xs={12} sm={6} md={4} lg={3}>
+                <Card className='p-4 fixed-height'>
+                <Card.Img variant="top" src= {product.imgSrc} />
+                <Card.Body>
+                <Card.Title>{product.brand}</Card.Title>
+                <Card.Text>{product.model}</Card.Text>
+                <Card.Text>{product.price}</Card.Text>
+                </Card.Body>
+            </Card>
+            </Col>
+            )}
+        </Row>
+    </div>
+    </>
   );
 }
 
---
+++ src/components/Products.js
@@ -0,0 +1,14 @@
+const products = [
+        {brand: 'Nike', model: `Women's Air Max Bella 5 Training Shoe`, imgSrc: 'https://www.famousfootwear.com/blob/product-images/20000/55/07/2/55072_pair_medium.jpg', price: '$89.99',},
+        {brand: 'Nike', model: `Women's Air Max Excee Sneaker`, imgSrc: 'https://www.famousfootwear.com/blob/product-images/20000/96/85/8/96858_pair_medium.jpg', price: '$89.99',},
+        {brand: 'Nike', model: `Women's Air Max Excee Sneaker 2`, imgSrc: 'https://www.famousfootwear.com/blob/product-images/20000/73/44/5/73445_pair_medium.jpg', price: '$79.99',},
+        {brand: 'Nike', model: `Women's Air Max Excee Sneaker 3`, imgSrc: 'https://www.famousfootwear.com/blob/product-images/20000/73/42/8/73428_pair_medium.jpg', price: '$84.99',},
+        {brand: 'Converse', model: `Women's Chuck Taylor All Star Hi Lift Platform Sneaker`, imgSrc: 'https://www.famousfootwear.com/blob/product-images/20000/53/18/9/53189_pair_medium.jpg', price: '$74.99',},
+        {brand: 'Vans', model: `Women's Asher Slip On Sneaker`, imgSrc: 'https://www.famousfootwear.com/blob/product-images/20000/94/60/3/94603_pair_medium.jpg', price: '$64.99',},
+        {brand: 'Adidas', model: `Women's Racer TR21 Sneaker`, imgSrc: 'https://www.famousfootwear.com/blob/product-images/20000/71/23/7/71237_pair_medium.jpg', price: '$74.99',},
+        {brand: 'HEYDUDE', model: `Women's Wendy Casual Slip On`, imgSrc: 'https://www.famousfootwear.com/blob/product-images/20000/36/55/4/36554_pair_medium.jpg', price: '$59.99',},
+        {brand: 'Birkenstock', model: `Women's Arizona Footbed Sandal`, imgSrc: 'https://www.famousfootwear.com/blob/product-images/20000/51/64/0/51640_pair_medium.jpg', price: '$109.99',},
+        {brand: 'Vans', model: `Ward Pride Low Top Sneaker`, imgSrc: 'https://www.famousfootwear.com/blob/product-images/20000/58/10/3/58103_pair_medium.jpg', price: '$59.99',}
+        ];
+
+export default products;
