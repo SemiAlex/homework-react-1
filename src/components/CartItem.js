ade83d4700ad6cb5c7f8d02a8f9bede5215c1f8a
Alex <herbalpie@gmail.com>
2022-08-30 16:05:02 +0300

    updated total

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -5,7 +5,7 @@ function CartItem({product, addCount, reduceCount, removeFromCart}) {
     return <ListGroup.Item key={product.id}>
         {product.brand} {product.model} (${(product.price*product.count).toFixed(2)})
         <Badge bg="success" className='pointer' onClick={() => addCount(product.id)}>+</Badge>
-        <Badge bg="secondary" className='pointer' onClick={() => reduceCount(product.id)}>{product.count}</Badge>
+        <Badge bg="secondary" className='pointer'>{product.count}</Badge>
         <Badge bg="danger" className='pointer' onClick={() => reduceCount(product.id)}>-</Badge>
         <Badge bg="danger" className='pointer mgl' onClick={() => removeFromCart(product.id)}>remove</Badge>
     </ListGroup.Item>
